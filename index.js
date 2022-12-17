604ab99c00ef27d0fb491e5c90837ee7ff5d485f
Nasikh CL <nasikcl@gmail.com>
2022-12-18 01:36:39 +0530

    used body parser

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
 const port =8000;
+const bodyPaser = require('body-parser');
+
+
+app.use(bodyPaser.urlencoded({extended: true}));
+app.use(bodyPaser.json())
+
+const userArr = [
+    {
+        id:1,
+        name: 'nasik'
+    },
+    {
+        id:2,
+        name: 'mahmoo'
+    }
+]
+
+
 app.get('/', (req,res)=>{
     res.send('hello world')
+})
+
+
+
+app.get('/getUsers', (req,res)=>{
+    res.json(userArr)
 })
 
 
