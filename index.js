31830b0d21325531eba81ab3509cb652cae05939
Nasikh CL <nasikcl@gmail.com>
2022-12-19 12:59:18 +0530

    get, post, put, delete api's

--- index.js
+++ index.js
@@ -23,11 +23,35 @@ app.get('/', (req,res)=>{
     res.send('hello world')
 })
 
-
-
 app.get('/getUsers', (req,res)=>{
     res.json(userArr)
 })
+
+app.post("/createUser", (req, res) => {
+    userArr.push(req.body);
+    return res.json({ message: "User added successfully" });
+  });
+  
+  app.delete("/deleteUser", (req, res) => {
+    const idToBeDeleted = req.body.id;
+    const updatedArr = userArr.filter((elem) => elem.id != idToBeDeleted);
+    return res.json({ userArr: updatedArr });
+  });
+  
+  app.put("/updateUser", (req, res) => {
+    const idToBeUpdated = req.body.id;
+    const index = userArr.findIndex((elem) => elem.id == idToBeUpdated);
+    userArr[index] = req.body;
+    return res.json({ message: "User updated successfully" });
+  });
+  
+  app.get('/getUserById', (req, res) =>{
+    const userId = req.body.id;
+    const user = userArr.find((elem) => elem.id == userId);
+    return res.json({user : user});
+  })
+
+
 
 
 app.listen(port, ()=>{
