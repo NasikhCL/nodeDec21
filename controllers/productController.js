c1be8afe784dcfd9b36b917c224fe4aca6a1d6a8
Nasikh CL <nasikcl@gmail.com>
2022-12-26 18:07:53 +0530

    get all product/ get product by id  route added

--- controllers/productController.js
+++ controllers/productController.js
@@ -17,3 +17,38 @@ exports.createProduct = async(req,res)=>{
         })
     }
 }
+
+exports.getProducts = async(req,res)=>{
+    try{
+        const products = await Product.find();
+        console.log('products are : ', products);
+        if(products){
+            return res.status(200).json({message:"products fetch sucessfully", products: products})
+        }else{
+
+            // await Product.create(req.body)
+            return res.status(400).json({message:"no product found"})
+        }
+
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message:"internal server error"
+        })
+    }
+}
+
+exports.getProductById = async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const product = await Product.findById(productId);
+      if (!product) {
+        return res.status(400).json({ message: "No product found" });
+      }
+      return res.status(200).json({ product: product });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: err, message: "Internal Server Error" });
+    }
+  };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct } = require('../controllers/productController');
+const { createProduct, getProducts } = require('../controllers/productController');
 const { validateJWT } = require('../middleware/jwt');
 
 
 router.post('/create', validateJWT, createProduct);
-
+router.get('/getProducts',validateJWT, getProducts)
 module.exports = router
